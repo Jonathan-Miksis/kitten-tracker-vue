fdf181b74950172c84f4c34b28ca3038d61dc4e5
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-17 19:31:44 -0500

    boilerplate vue app

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        message: "Look at all of these kittens!"
+      };
+    },
+    created: function() {},
+    methods: {},
+  };
+</script>
