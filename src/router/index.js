5e898dcceeff1a73f85fe6282641cf8fff103e92
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-17 22:04:47 -0500

    add show action/route for users

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import KittensIndex from '../views/KittensIndex.vue'
 import KittensNew from '../views/KittensNew.vue'
+import KittensShow from '../views/KittensShow.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/kittens/new',
     name: 'KittensNew',
     component: KittensNew
+  },
+  {
+    path: '/kittens/:id',
+    name: 'KittensShow',
+    component: KittensShow
   },
   {
     path: '/about',
---
+++ src/views/KittensShow.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="show-kitten">
+    <p>Name: {{ kitten.name }}</p>
+    <p>Age: {{ kitten.age }}</p>
+    <p>Color: {{ kitten.color }}</p>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from 'axios'
+
+  export default {
+    data: function () {
+      return {
+        kitten: {},
+      };
+    },
+    created: function () {
+      axios.get(`/kittens/${this.$route.params.id}`).then(response => {
+        console.log(response.data);
+        this.kitten = response.data
+      })
+    },
+    methods: {},
+  };
+</script>
